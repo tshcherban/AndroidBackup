56a21b32f66de634bfedffd6e4204619e4dfecac
tshcherban <shcherban91@gmail.com>
2017-10-24 17:35:51 +0300

    Extracted client implementation

--- src/CustomClient/Program.cs
+++ src/CustomClient/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Security.Cryptography;
-using FileSync.Common;
-using ServiceWire.TcpIp;
+﻿using FileSync.Common;
 
 namespace FileSync.TestClient
 {
@@ -12,48 +6,8 @@ namespace FileSync.TestClient
     {
         static void Main(string[] args)
         {
-
-            using (var client = new TcpClient<ISyncFileService>(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9211)))
-            {
-                var proxy = client.Proxy;
-                const string baseDir = @"G:\SyncTest\Src";
-
-                var localFiles = Directory.GetFiles(baseDir, "*", SearchOption.AllDirectories);
-                var localInfos = localFiles.Select(i =>
-                {
-                    HashAlgorithm alg = SHA1.Create();
-                    alg.ComputeHash(File.OpenRead(i));
-                    return new SyncFileInfo { Hash = alg.Hash, RelativePath = i.Replace(baseDir, string.Empty) };
-                }).ToList();
-
-                var sessionId = proxy.GetSession();
-
-                Console.WriteLine($"Got session {sessionId}");
-                Console.ReadKey();
-
-                var syncList = proxy.GetSyncList(sessionId, localInfos);
-
-                Console.WriteLine($"Got sync list. {syncList.ToUpload} to upload");
-                Console.ReadKey();
-
-                foreach (var i in syncList.ToUpload)
-                {
-                    var f = baseDir + i.RelativePath;
-                    var file = File.ReadAllBytes(f);
-                    proxy.SendFile(sessionId, i.RelativePath, i.Hash, file);
-
-                    Console.WriteLine($"{i.RelativePath} sent");
-                }
-
-                Console.WriteLine($"About to complete session");
-                Console.ReadKey();
-
-                proxy.CompleteSession(sessionId);
-
-                Console.WriteLine($"Session completed");
-
-                Console.ReadKey();
-            }
+            var client = SyncClientFactory.GetOneWaySyncClient("127.0.0.1", 9211, @"G:\SyncTest\Src");
+            client.Sync();
         }
     }
 }
---
+++ src/FileSync.Common/OneWaySyncClientImpl.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using ServiceWire.TcpIp;
+
+namespace FileSync.Common
+{
+    public static class SyncClientFactory
+    {
+        public static ISyncClient GetOneWaySyncClient(string serverAddress, int serverPort, string baseDir)
+        {
+            return new OneWaySyncClientImpl(serverAddress, serverPort, baseDir);
+        }
+    }
+
+    public interface ISyncClient
+    {
+        void Sync();
+    }
+
+    internal sealed class OneWaySyncClientImpl : ISyncClient
+    {
+        private readonly string _serverAddress;
+        private readonly int _serverPort;
+        private readonly string _baseDir;
+
+        public OneWaySyncClientImpl(string serverAddress, int serverPort, string baseDir)
+        {
+            _serverAddress = serverAddress;
+            _serverPort = serverPort;
+            _baseDir = baseDir;
+        }
+
+        public void Sync()
+        {
+            using (var client = new TcpClient<ISyncFileService>(new IPEndPoint(IPAddress.Parse(_serverAddress), _serverPort)))
+            {
+                var proxy = client.Proxy;
+
+                var localFiles = Directory.GetFiles(_baseDir, "*", SearchOption.AllDirectories);
+                var localInfos = localFiles.Select(i =>
+                {
+                    HashAlgorithm alg = SHA1.Create();
+                    alg.ComputeHash(File.OpenRead(i));
+                    return new SyncFileInfo { Hash = alg.Hash, RelativePath = i.Replace(_baseDir, string.Empty) };
+                }).ToList();
+
+                var sessionId = proxy.GetSession();
+
+                var syncList = proxy.GetSyncList(sessionId, localInfos);
+
+                foreach (var i in syncList.ToUpload)
+                {
+                    var f = _baseDir + i.RelativePath;
+                    var file = File.ReadAllBytes(f);
+                    proxy.SendFile(sessionId, i.RelativePath, i.Hash, file);
+                }
+
+                proxy.CompleteSession(sessionId);
+            }
+        }
+    }
+}
